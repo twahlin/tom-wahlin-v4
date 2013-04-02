00b759208eb0ef55eb24775becd80242578a9794
Tom Wahlin <tom.wahlin@gmail.com>
2013-04-02 16:46:11 -0400

    Initialized blog

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source :rubygems
 gem "middleman", "~>3.0.9"
 
 gem "middleman-livereload"
+
+gem "middleman-blog"
+
+gem "redcarpet"
--- config.rb
+++ config.rb
@@ -76,11 +76,27 @@ configure :build do
   # set :http_path, "/Content/images/"
 end
 
+### Customized Below ###
+
+set :markdown_engine, :redcarpet
+set :markdown,  :fenced_code_blocks => true,
+                :autolink => true, 
+                :smartypants => true
+
+# Initializing the blog
+activate :blog do |blog|
+  blog.prefix = "projects"
+  blog.permalink = ":title"
+  blog.layout = "blog"
+  blog.default_extension = ".markdown"
+end
+
 # Custom layout for project pages - these are the main projects in the portfolio
 page "/projects/*", :layout => "projects"
 
 # Enables pretty urls without the file extension
 activate :directory_indexes
+
 
 # livereload used for instant coding
 activate :livereload
