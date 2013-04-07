4650e078fdd0ca722b864753041796213caf9dbd
Tom Wahlin <tom.wahlin@gmail.com>
2013-04-07 10:58:08 -0400

    Blog properly set

--- config.rb
+++ config.rb
@@ -1,4 +1,33 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # blog.prefix = "projects"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page "/feed.xml", :layout => false
+
+###
 # Compass
 ###
 
@@ -77,9 +106,6 @@ configure :build do
 end
 
 ### Customized Below ###
-
-# Custom layout for project pages - these are the main projects in the portfolio
-page "/projects/*", :layout => "projects"
 
 # Enables pretty urls without the file extension
 activate :directory_indexes
---
+++ source/2013-01-02-example-two.html.markdown
@@ -0,0 +1,7 @@
+---
+title: Example Article Dos
+date: 2013-01-02
+tags: blah
+---
+
+Another one!
