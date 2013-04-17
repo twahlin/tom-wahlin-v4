509d4a28f7d1f24b30c00b360a4abbb17af699ae
Tom Wahlin <tom.wahlin@gmail.com>
2013-04-17 19:29:04 -0400

    Responsive image testing

--- source/js/main.js
+++ source/js/main.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
 		$('.masthead').toggleClass('nav-push');
 		$('.masthead-wrap nav ul li a').toggleClass('nav-open');
 	});
+	
+	// responsive images, also @2x
+	$.fn.imageSwap({imageContainer: '.img-project', breakpoints: [320,321,960]})
 
-	//hide safari bar mobile web
+	//hide safari bar mobile web - this should be edited, not great with mobile chrome
 	window.addEventListener("load",function() {
 		// Set a timeout...
 		setTimeout(function(){
