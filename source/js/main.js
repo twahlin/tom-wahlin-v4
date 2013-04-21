bfeb48081122b2e3205e8bed7b738c2f37a789cd
Tom Wahlin <tom.wahlin@gmail.com>
2013-04-20 20:04:44 -0400

    Populating the content for home pages. Rethinking how responsive images will be implemented

--- source/js/main.js
+++ source/js/main.js
@@ -4,16 +4,7 @@ $(document).ready(function() {
 	$('.nav-toggle').click(function () {
 		$('.masthead').toggleClass('nav-push');
 		$('.masthead-wrap nav ul li a').toggleClass('nav-open');
-	});
-	
-	// responsive images, also @2x
-	$.fn.imageSwap({imageContainer: '.img-project', loadBestAvailable: true, monitorPixelDensity: true, breakpoints: [320,321,960]})
-	$.fn.imageSwap({imageContainer: '.img-hero-preview', loadBestAvailable: true, monitorPixelDensity: true, breakpoints: [320,480]})
-	
-	
-	
-	
-	
+	});	
 
 	//hide safari bar mobile web - this should be edited, not great with mobile chrome
 	window.addEventListener("load",function() {
@@ -23,4 +14,9 @@ $(document).ready(function() {
 			window.scrollTo(0, 1);
 			}, 0);
 		});	
+});
+
+
+$(window).load(function () {
+
 });
